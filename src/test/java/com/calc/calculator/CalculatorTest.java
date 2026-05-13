4fd95a6663e61441ff4bcc9b920b29b20f7032ce
axy-n <axy-n@ubuntu.sys>
2026-05-14 01:30:34 +0800

    Cherry pick test before removing them

--- src/test/java/com/calc/calculator/CalculatorTest.java
+++ src/test/java/com/calc/calculator/CalculatorTest.java
@@ -1,27 +1,20 @@
 package com.calc.calculator;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
     @Test
-    void sumTestAddTwoNumbersExpectSum() {
+    void sum_addTwoNumbers_expectSum() {
         Calculator calculator = new Calculator();
         assertEquals(5, calculator.sum(2, 3));
 
     }
 
     @Test
-    void sumTestAddZeroToNumberExpectNumber() {
+    void sum_addZeroToNumber_ExpectNumber() {
         Calculator calculator = new Calculator();
         assertEquals(5, calculator.sum(0, 5));
-    }
-
-    @Test
-    void differenceTestAddTwoNumbersExpectDifference() {
-        Calculator calculator = new Calculator();
-        assertEquals(1, calculator.difference(6, 5));
     }
 }
