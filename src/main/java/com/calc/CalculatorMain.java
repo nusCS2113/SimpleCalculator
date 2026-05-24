481bba912e3544aae70e72581cfc55bbb85a73e4
axy-n <axy-n@ubuntu.sys>
2026-05-24 15:37:27 +0800

    Refactor calculator

--- src/main/java/com/calc/CalculatorMain.java
+++ src/main/java/com/calc/CalculatorMain.java
@@ -1,10 +1,20 @@
 package com.calc;
 
+import com.calc.history.HistoryManager;
+import com.calc.storage.FileStorage;
 import com.calc.ui.CalculatorUI;
 
 public class CalculatorMain {
+    static HistoryManager historyManager = new HistoryManager();
+    static FileStorage fileStorage = new FileStorage("calculator_history.txt");
+
     public static void main(String[] args) {
-        CalculatorUI ui = new CalculatorUI();
-        ui.start();
+       run();
+    }
+
+    public static void run() {
+        historyManager = new HistoryManager();
+        CalculatorUI ui = new CalculatorUI(historyManager, fileStorage);
+        ui.runRepl();
     }
 }
