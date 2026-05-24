ebdf1bf942d39d6193193dd5cd5bc923cdd3a942
axy-n <axy-n@ubuntu.sys>
2026-05-24 23:45:18 +0800

    Ensure history file is erased every session
    make file writer append = false

--- src/main/java/com/calc/storage/FileStorage.java
+++ src/main/java/com/calc/storage/FileStorage.java
@@ -11,7 +11,7 @@ public class FileStorage {
     }
 
     public void saveRecord(String record) {
-        try (FileWriter writer = new FileWriter(fileName, true)) {
+        try (FileWriter writer = new FileWriter(fileName, false)) {
             writer.write(record + System.lineSeparator());
         } catch (IOException e) {
             System.out.println("Could not save record: " + e.getMessage());
