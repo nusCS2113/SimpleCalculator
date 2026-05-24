2b41eeddedf10b40d43fa491c61d8ea538146285
axy-n <axy-n@ubuntu.sys>
2026-05-24 22:47:29 +0800

    Make history manager singleton; use singleton in Main

--- src/main/java/com/calc/CalculatorMain.java
+++ src/main/java/com/calc/CalculatorMain.java
@@ -5,7 +5,7 @@ import com.calc.storage.FileStorage;
 import com.calc.ui.CalculatorUI;
 
 public class CalculatorMain {
-    static HistoryManager historyManager = new HistoryManager();
+    static HistoryManager historyManager;
     static FileStorage fileStorage = new FileStorage("calculator_history.txt");
 
     public static void main(String[] args) {
@@ -13,8 +13,8 @@ public class CalculatorMain {
     }
 
     public static void run() {
-        historyManager = new HistoryManager();
-        CalculatorUI ui = new CalculatorUI(historyManager, fileStorage);
+        historyManager = HistoryManager.getInstance();
+        CalculatorUI ui = new CalculatorUI(fileStorage);
         ui.runRepl();
     }
 }
